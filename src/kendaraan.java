14e90aa4558326658558513652a2129561f2c716
HNadhif <ub.haidarnadhifathaputra@gmail.com>
2025-11-06 17:35:23 +0700

    Membenarkan grafik dan menambahkan class abstract kendaraan

---
+++ src/kendaraan.java
@@ -0,0 +1,48 @@
+package src;
+
+public abstract class kendaraan {
+    // Atribut
+    private String id;
+    private String nama;
+    private double biayaHarian;
+    private boolean tersedia;
+
+    // Constructor
+    public kendaraan(String id, String nama, double biayaHarian, boolean tersedia) {
+        this.id = id;
+        this.nama = nama;
+        this.biayaHarian = biayaHarian;
+        this.tersedia = tersedia;
+    }
+
+    // Method
+    public String getID() {
+        return this.id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public double getBiayaHarian() {
+        return this.biayaHarian;
+    }
+
+    public boolean isTersedia() {
+        return this.tersedia;
+    }
+
+    public double hitungBiayaTotal(int hari) {
+        return hari * this.biayaHarian;
+    }
+
+    public void tampilkanDetail() {
+        System.out.println( "ID: " + this.id +
+                            "\nNama: " + this.nama +
+                            "\nBiaya Harian: " + this.biayaHarian +
+                            "\nStatus: " + (this.tersedia ? "Tersedia" : "Tidak Tersedia"));
+    }
+
+    // Method abstract
+    protected abstract double hitungBiayaTambahan();
+}
