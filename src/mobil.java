844a527978c73994c2b190e253e65b33390c8a33
HNadhif <ub.haidarnadhifathaputra@gmail.com>
2025-11-06 20:03:24 +0700

    Menambahkan class mobil.java

--- src/mobil.java
+++ src/mobil.java
@@ -1,5 +1,18 @@
 package src;
 
-public class mobil {
-    // Yang Mengerjakan : Haidar
+public class mobil extends kendaraan {
+    // Atribut
+    private boolean bensin;
+
+    // Construction
+    public mobil(String id, String nama, double biayaHarian, boolean tersedia, boolean bensin) {
+        super(id, nama, biayaHarian, tersedia);
+        this.bensin = bensin;
+    }
+
+    // Method
+    @Override
+    protected double hitungBiayaTambahan() {
+        return (bensin? getBiayaHarian()*0.1 : 0);
+    }
 }
