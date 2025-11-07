236baea32523949c9736d64b9517635824231da1
winnoll <aikowinnola1@gmail.com>
2025-11-07 07:13:31 +0700

    pembuatan objek motor

--- src/motor.java
+++ src/motor.java
@@ -1,4 +1,13 @@
 package src;
 
-public class motor {
+public class motor extends kendaraan{
+    public motor(String id, String nama, double biayaHarian, boolean tersedia) {
+        super(id, nama, biayaHarian, tersedia);
+
+    }
+
+    @Override
+    protected double hitungBiayaTambahan(){
+        return 20000;
+    }
 }
