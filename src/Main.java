990399122a82c436782ec1ec7b3a8f28d6fd0627
HNadhif <ub.haidarnadhifathaputra@gmail.com>
2025-11-10 06:56:41 +0700

    Progress main sampai menambahkan 2 objek

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 package src;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main (String[] args) {
-        // Kita bahas ya nanti
+        // Inisialisasi
+        ArrayList<kendaraan> inventaris = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        // Membuat kendaraan sebanyak 7 objek
+        kendaraan M1 = new mobil("M-0001", "Avanza", 250000, true, false);
+        kendaraan M2 = new mobil("M-0002", "Honda", 300000, true, true);
+
     }
 }
